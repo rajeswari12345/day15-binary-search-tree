c6f573f1c12aa4e059551a07cac1c41b4c0087fd
mani1 <mani1@NANI143>
2022-07-02 14:28:06 +0530

    binary search tree

---
+++ day 15 binary seach tree/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class BinarySearchTree
+{
+
+	// Class containing left and
+	// right child of current node
+	// and key value
+	public class Node
+	{
+		public int key;
+		public Node left, right;
+
+		public Node(int item)
+		{
+			key = item;
+			left = right = null;
+		}
+	}
+
+	// Root of BST
+	Node root;
+
+	// Constructor
+	BinarySearchTree() { root = null; }
+
+	BinarySearchTree(int value) { root = new Node(value); }
+
+	// This method mainly calls insertRec()
+	void insert(int key) { root = insertRec(root, key); }
+
+	// A recursive function to insert
+	// a new key in BST
+	Node insertRec(Node root, int key)
+	{
+
+		// If the tree is empty,
+		// return a new node
+		if (root == null)
+		{
+			root = new Node(key);
+			return root;
+		}
+
+		// Otherwise, recur down the tree
+		if (key < root.key)
+			root.left = insertRec(root.left, key);
+		else if (key > root.key)
+			root.right = insertRec(root.right, key);
+
+		// Return the (unchanged) node pointer
+		return root;
+	}
+
+	// This method mainly calls InorderRec()
+	void inorder() { inorderRec(root); }
+
+	// A utility function to
+	// do inorder traversal of BST
+	void inorderRec(Node root)
+	{
+		if (root != null)
+		{
+			inorderRec(root.left);
+			Console.WriteLine(root.key);
+			inorderRec(root.right);
+		}
+	}
+
+	// Driver Code
+	public static void Main(String[] args)
+	{
+		BinarySearchTree tree = new BinarySearchTree();
+
+
+		tree.insert(56);
+		tree.insert(30);
+		tree.insert(22);
+		tree.insert(11);
+		tree.insert(3);
+		tree.insert(16);
+		tree.insert(40);
+		tree.insert(70);
+		tree.insert(60);
+		tree.insert(65);
+		tree.insert(63);
+		tree.insert(67);
+		tree.insert(95);
+
+		// Print inorder traversal of the BST
+		tree.inorder();
+	}
+}
+
+
---
+++ day 15 binary search uc2/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+
+public class Node
+{
+	public int data;
+	public Node left, right;
+
+	public Node(int item)
+	{
+		data = item;
+		left = right = null;
+	}
+}
+
+
+public class BinaryTree
+{
+	public Node root;
+
+
+	public virtual int size()
+	{
+		return size(root);
+	}
+
+
+	public virtual int size(Node node)
+	{
+		if (node == null)
+		{
+			return 0;
+		}
+		else
+		{
+			return (size(node.left) + 1 + size(node.right));
+		}
+	}
+
+	public static void Main(string[] args)
+	{
+		BinarySearchTree tree = new BinarySearchTree();
+
+		tree.insert(56);
+		tree.insert(30);
+		tree.insert(22);
+		tree.insert(11);
+		tree.insert(3);
+		tree.insert(16);
+		tree.insert(40);
+		tree.insert(70);
+		tree.insert(60);
+		tree.insert(65);
+		tree.insert(63);
+		tree.insert(67);
+		tree.insert(95);
+
+		Console.WriteLine("The size of binary tree is : " + tree.size());
+	}
+}
